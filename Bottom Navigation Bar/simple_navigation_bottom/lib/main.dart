5f2f5526f9f02b46f793ff680c121f1c8383b4ef
Rahman Bayramov <bayramovrahman@gmail.com>
2024-06-17 09:52:45 +0500

    Flutter Navigation Bar

---
+++ Bottom Navigation Bar/simple_navigation_bottom/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:simple_navigation_bottom/pages/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Navigation Buttom",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
